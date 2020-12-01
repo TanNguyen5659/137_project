cf89ebe8468db9a4da52c5c9e62680264f999671
Tan Nguyen <tannguyen5659@gmail.com>
2020-11-30 23:10:55 -0800

    added constraints for multiple choice view

--- 137_project/Controller/MultipleChoiceViewController.swift
+++ 137_project/Controller/MultipleChoiceViewController.swift
@@ -1,14 +1,14 @@
 //
 //  MultipleChoiceViewController.swift
-//  137_project
+//  Quiz Game
 //
-//  Created by Tan Nguyen on 11/30/20.
+//  Created by Johannes Ruof on 13/11/2016.
+//  Copyright © 2016 Rume Academy. All rights reserved.
 //
 
 import UIKit
 
 class MultipleChoiceViewController: UIViewController {
-
     private let contentView = UIView()
     private var contentViewConstraints: [NSLayoutConstraint]!
     
@@ -34,10 +34,11 @@ class MultipleChoiceViewController: UIViewController {
     
     private let backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
     private let foregroundColor = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = backgroundColor
+        layoutView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -75,5 +76,94 @@ class MultipleChoiceViewController: UIViewController {
         contentView.addSubview(countdownView)
         progressView.translatesAutoresizingMaskIntoConstraints = false
         countdownView.addSubview(progressView)
+        
+        contentViewConstraints = [
+            contentView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        questionViewConstraints = [
+            questionView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20.0),
+            questionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20.0),
+            questionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20.0),
+            questionView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.4)
+        ]
+        
+        questionLabelConstraints = [
+            questionLabel.topAnchor.constraint(equalTo: questionView.topAnchor),
+            questionLabel.leadingAnchor.constraint(equalTo: questionView.leadingAnchor),
+            questionLabel.trailingAnchor.constraint(equalTo: questionView.trailingAnchor),
+            questionLabel.bottomAnchor.constraint(equalTo: questionView.bottomAnchor)
+        ]
+        
+        questionButtonConstraints = [
+            questionButton.topAnchor.constraint(equalTo: questionView.topAnchor),
+            questionButton.leadingAnchor.constraint(equalTo: questionView.leadingAnchor),
+            questionButton.trailingAnchor.constraint(equalTo: questionView.trailingAnchor),
+            questionButton.bottomAnchor.constraint(equalTo: questionView.bottomAnchor)
+        ]
+        
+        answerViewConstraints = [
+            answerView.topAnchor.constraint(equalTo: questionView.bottomAnchor, constant: 20.0),
+            answerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20.0),
+            answerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20.0),
+            answerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.4)
+        ]
+        
+        answerButtonsConstraints = [
+            answerButtons[0].leadingAnchor.constraint(equalTo: answerView.leadingAnchor),
+            answerButtons[0].trailingAnchor.constraint(equalTo: answerButtons[1].leadingAnchor, constant: -8.0),
+            answerButtons[0].topAnchor.constraint(equalTo: answerView.topAnchor),
+            answerButtons[0].bottomAnchor.constraint(equalTo: answerButtons[2].topAnchor, constant: -8.0),
+            answerButtons[1].trailingAnchor.constraint(equalTo: answerView.trailingAnchor),
+            answerButtons[1].topAnchor.constraint(equalTo: answerView.topAnchor),
+            answerButtons[1].bottomAnchor.constraint(equalTo: answerButtons[3].topAnchor, constant: -8.0),
+            answerButtons[2].leadingAnchor.constraint(equalTo: answerView.leadingAnchor),
+            answerButtons[2].trailingAnchor.constraint(equalTo: answerButtons[3].leadingAnchor, constant: -8.0),
+            answerButtons[2].bottomAnchor.constraint(equalTo: answerView.bottomAnchor),
+            answerButtons[3].trailingAnchor.constraint(equalTo: answerView.trailingAnchor),
+            answerButtons[3].bottomAnchor.constraint(equalTo: answerView.bottomAnchor)
+        ]
+        
+        for index in 1..<answerButtons.count {
+            answerButtonsConstraints.append(answerButtons[index].heightAnchor.constraint(equalTo: answerButtons[index-1].heightAnchor))
+            answerButtonsConstraints.append(answerButtons[index].widthAnchor.constraint(equalTo: answerButtons[index-1].widthAnchor))
+        }
+        
+        countdownViewConstraints = [
+            countdownView.topAnchor.constraint(equalTo: answerView.bottomAnchor, constant: 20.0),
+            countdownView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20.0),
+            countdownView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20.0),
+            countdownView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20.0)
+        ]
+        
+        progressViewConstraints = [
+            progressView.leadingAnchor.constraint(equalTo: countdownView.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: countdownView.trailingAnchor),
+            progressView.centerYAnchor.constraint(equalTo: countdownView.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(contentViewConstraints)
+        NSLayoutConstraint.activate(questionViewConstraints)
+        NSLayoutConstraint.activate(questionLabelConstraints)
+        NSLayoutConstraint.activate(questionButtonConstraints)
+        NSLayoutConstraint.activate(answerViewConstraints)
+        NSLayoutConstraint.activate(answerButtonsConstraints)
+        NSLayoutConstraint.activate(countdownViewConstraints)
+        NSLayoutConstraint.activate(progressViewConstraints)
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 }
